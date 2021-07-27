c2711086c460bf8c08a39b061367b6d10e2e1a7b
Stackbit <projects@stackbit.com>
2021-07-27 05:30:28 +0000

    initial commit by Stackbit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Log-in-sign-up-form
+* download all files with images in your computer.
+* look there are two html files.
+* open log-in.html file first
---
+++ scrpt.js
@@ -0,0 +1,17 @@
+
+function verify(){
+    var a = prompt('Enter Your age');
+    var b = 18;
+    if (a >= b){
+           c = document.getElementById('ahead');
+        }
+        else{
+            alert('You are Not eligible');
+        }
+      
+    
+
+        console.log(a , b);
+    }
+
+
